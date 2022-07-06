d6f142b9d9ceaa49fd5d8529eaeb01df790d3227
Reza Pahlevi Haikal <eskrimayambawang@gmail.com>
2022-07-06 14:51:45 +0700

    update environtment #2

--- application/storage/framework/views/c5982f7bf89a4d6b9e61a87865d13a4b8bf1acd9.php
+++
@@ -1,7 +0,0 @@
-<?php if(auth()->user()->is_team): ?>
-<?php echo $__env->make('nav.leftmenu-team', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php endif; ?>
-
-<?php if(auth()->user()->is_client): ?>
-<?php echo $__env->make('nav.leftmenu-client', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php endif; ?><?php /**PATH /opt/lampp/htdocs/crm/application/resources/views/nav/leftmenu.blade.php ENDPATH**/ ?>
